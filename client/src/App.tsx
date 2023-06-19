f76942f09c7b3ae2086ca560b78c853b5044a1c1
idgidikov <idgidikov@abv.bg>
2023-06-19 14:33:04 +0300

    feat : list all todos func

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,65 @@
-import { Box } from "@mantine/core";
-import useSWR from 'swr';
-import AddTodo from "./components/AddTodo"
-
+import { Box, List, ThemeIcon } from "@mantine/core";
+import useSWR from "swr";
+import AddTodo from "./components/AddTodo";
+import { CheckCircleFillIcon } from "@primer/octicons-react";
 export interface Todo {
   id: number;
   title: string;
-  body : string;
-  done : boolean;
-
+  body: string;
+  done: boolean;
 }
 
-export const ENDPOINT = 'http://localhost:4000'
-const fetcher = (url : string) => fetch(`${ENDPOINT}/${url}`).then((r)=> r.json());
+export const ENDPOINT = "http://localhost:4000";
+const fetcher = (url: string) =>
+  fetch(`${ENDPOINT}/${url}`).then((r) => r.json());
 function App() {
- 
-  const {data, mutate} = useSWR<Todo[]>('api/todos', fetcher)
+  const { data, mutate } = useSWR<Todo[]>("api/todos", fetcher);
 
-  return (<>
-    <Box>{JSON.stringify(data)}</Box>
-    <AddTodo mutate = {mutate}/>
+  async function markTodoAdDone(id: number) {
+    const updated = await fetch(`${ENDPOINT}/api/todos/${id}/done`, {
+      method: "PATCH",
+    }).then((r) => r.json());
+
+    mutate(updated);
+  }
+
+  return (
+    <>
+      <Box
+        sx={(theme) => ({
+          padding: "2rem",
+          width: "100%",
+          maxWidth: "40rem",
+          margin: "0 auto",
+        })}
+      >
+        <List spacing="xs" size="sm" mb={12} center>
+          {data?.map((todo) => {
+            return (
+              <List.Item
+                key={`todo__${todo.id}`}
+                onClick={() => markTodoAdDone(todo.id)}
+                icon={
+                  todo.done ? (
+                    <ThemeIcon color="teal" size="24" radius="xl">
+                      <CheckCircleFillIcon size={20} />
+                    </ThemeIcon>
+                  ) : (
+                    <ThemeIcon color="gray" size="24" radius="xl">
+                      <CheckCircleFillIcon size={20} />
+                    </ThemeIcon>
+                  )
+                }
+              >
+                {todo.title}
+              </List.Item>
+            );
+          })}
+        </List>
+        <AddTodo mutate={mutate} />
+      </Box>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- server/main.go
+++ server/main.go
@@ -2,56 +2,58 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
-
-	"log"
 )
 
 type Todo struct {
-	ID int `json:"id"`
+	ID    int    `json:"id"`
 	Title string `json:"title"`
-	Done bool `json:"done"`
-	Body string `json:"body"`
+	Done  bool   `json:"done"`
+	Body  string `json:"body"`
 }
 
-func main(){
+func main() {
 	fmt.Print("Hello world")
 
 	app := fiber.New()
 
 	app.Use(cors.New(cors.Config{
-		AllowOrigins : "http://localhost:5173",
-		AllowHeaders : "Origin, Content-Type, Accept",
+		AllowOrigins: "http://localhost:5173",
+		AllowHeaders: "Origin, Content-Type, Accept",
 	}))
 
 	todos := []Todo{}
 
-	app.Get("/healthcheck" , func(c *fiber.Ctx) error {
+	app.Get("/healthcheck", func(c *fiber.Ctx) error {
 		return c.SendString("OK")
 	})
 
 	app.Post("/api/todos", func(c *fiber.Ctx) error {
 		todo := &Todo{}
-		 if err := c.BodyParser(todo); err!=nil {
+
+		if err := c.BodyParser(todo); err != nil {
 			return err
-		 }
-		 
-		 todo.ID = len(todos) + 1
+		}
 
-		 todos = append(todos, *todo)
+		todo.ID = len(todos) + 1
 
-		 return c.JSON(todos)
+		todos = append(todos, *todo)
+
+		return c.JSON(todos)
+
 	})
-	app.Patch("/api/todos/:id/done" ,func(c *fiber.Ctx) error {
-		id , err := c.ParamsInt("id")
+
+	app.Patch("/api/todos/:id/done", func(c *fiber.Ctx) error {
+		id, err := c.ParamsInt("id")
 
 		if err != nil {
 			return c.Status(401).SendString("Invalid id")
 		}
-		
-		for i , t := range todos {
+
+		for i, t := range todos {
 			if t.ID == id {
 				todos[i].Done = true
 				break
@@ -59,13 +61,9 @@ func main(){
 		}
 
 		return c.JSON(todos)
-
-
 	})
 
-
-	app.Get("/api/todos" , func(c *fiber.Ctx) error {
-
+	app.Get("/api/todos", func(c *fiber.Ctx) error {
 		return c.JSON(todos)
 	})
 
